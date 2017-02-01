19eede60cfd339d7aa8532519c2b9965cc7ca2b0
Han <hanshengzhao1993@gmail.com>
2017-02-01 00:18:27 -0800

    Inserting more information into the page

--- js/main.js
+++ js/main.js
@@ -11,13 +11,35 @@ btn.addEventListener('click', function () {
   }
   ourRequest.send();  
   pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add('hide-me')
+  }
 });
 
 var renderHtml = function (data) {
   var htmlStirng = '';
 
   for(var i = 0; i<data.length; i++){
-    htmlStirng += "<p>" + data[i].name + ' is a '+ data[i].species + "</p>";
+    htmlStirng += "<p>" + data[i].name + ' is a '+ data[i].species + " likes to eat ";
+
+    for(var ii =0; ii<data[i].foods.likes.length; ii++){
+      if (ii === data[i].foods.likes.length - 1) {
+        htmlStirng += data[i].foods.likes[ii];
+      } else {
+        htmlStirng += data[i].foods.likes[ii] + " and ";
+      }
+    }
+    htmlStirng += ' and dislikes ';
+
+    for(var ii =0; ii<data[i].foods.dislikes.length; ii++){
+      if (ii === data[i].foods.dislikes.length - 1) {
+        htmlStirng += data[i].foods.dislikes[ii];
+      } else {
+        htmlStirng += data[i].foods.dislikes[ii] + " and ";
+      }
+    }
+
+    htmlStirng += '.</p>';
   }
 
   animalContainer.insertAdjacentHTML('beforeend', htmlStirng)
